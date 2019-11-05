35babcf6d3616e2b95f1d5c4c0b871589c020cb3
Manali Patil <mmpatil@dons.usfca.edu>
2019-11-04 18:10:35 -0800

    Working on Travel Startup

---
+++ src/main/java/edu/usfca/cs/mr/travelStartup/TravelStartupMapper.java
@@ -0,0 +1,14 @@
+package edu.usfca.cs.mr.travelStartup;
+
+import edu.usfca.cs.mr.util.NCDCWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class TravelStartupMapper extends Mapper<LongWritable,Text, Text, NCDCWritable> {
+
+
+
+
+
+}
---
+++ src/main/java/edu/usfca/cs/mr/travelStartup/TravelStartupReducer.java
@@ -0,0 +1,12 @@
+package edu.usfca.cs.mr.travelStartup;
+
+import edu.usfca.cs.mr.util.NCDCWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class TravelStartupReducer extends Reducer<Text, NCDCWritable,Text,Text> {
+
+
+
+}
